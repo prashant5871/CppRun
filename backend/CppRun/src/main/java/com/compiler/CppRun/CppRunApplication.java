f990816e2e25d9dc44ceb87169df1361b231afb8
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-06-11 11:50:53 +0530

    frontend and backend project initlized

---
+++ backend/CppRun/src/main/java/com/compiler/CppRun/CppRunApplication.java
@@ -0,0 +1,13 @@
+package com.compiler.CppRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CppRunApplication.class, args);
+	}
+
+}
